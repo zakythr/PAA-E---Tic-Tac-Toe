62bc28b2bd8e4fa35046e00a93de8752aeeeeab4
4rMyMen <36811824+4rMyMen@users.noreply.github.com>
2019-03-30 13:34:22 +0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -80,3 +80,42 @@ class Board:
     button = Button(self.app, text='reset', command=handler)
     button.grid(row=self.board.size+1, column=0, columnspan=self.board.size, sticky="WE")
     self.update()
+
+    def reset(self):
+    self.board = Board()
+    self.update()
+  
+  def move(self,x,y):
+    self.app.config(cursor="watch")
+    self.app.update()
+    self.board = self.board.move(x,y)
+    self.update()
+    move = self.board.best()
+    if move:
+      self.board = self.board.move(*move)
+      self.update()
+    self.app.config(cursor="")
+            
+  def update(self):
+    for (x,y) in self.board.fields:
+      text = self.board.fields[x,y]
+      self.buttons[x,y]['text'] = text
+      self.buttons[x,y]['disabledforeground'] = 'black'
+      if text==self.board.empty:
+        self.buttons[x,y]['state'] = 'normal'
+      else:
+        self.buttons[x,y]['state'] = 'disabled'
+    winning = self.board.won()
+    if winning:
+      for x,y in winning:
+        self.buttons[x,y]['disabledforeground'] = 'red'
+      for x,y in self.buttons:
+        self.buttons[x,y]['state'] = 'disabled'
+    for (x,y) in self.board.fields:
+      self.buttons[x,y].update()
+
+  def mainloop(self):
+    self.app.mainloop()
+
+if __name__ == '__main__':
+  GUI().mainloop()
